76f9d5e96dedbdc11336db719a2ec7bfe1df7cc9
Richard Kweku Aikins <richardkns7@gmail.com>
2023-12-15 05:57:00 +0000

    inquiry error handled

--- app/Http/Controllers/InquiryController.php
+++ app/Http/Controllers/InquiryController.php
@@ -15,30 +15,35 @@ class InquiryController extends Controller
 
     public function submitForm(Request $request)
     {
-        $validatedData = $request->validate([
-            'first_name' => 'required|string',
-            'last_name' => 'required|string',
-            'email' => 'required|email',
-            'subject' => 'required|string',
-            'message' => 'required|string',
-        ]);
+        try {
+            $validatedData = $request->validate([
+                'first_name' => 'required|string',
+                'last_name' => 'required|string',
+                'email' => 'required|email',
+                'subject' => 'required|string',
+                'message' => 'required|string',
+            ]);
 
-        // Inquiry::create($validatedData);
+            // Inquiry::create($validatedData);
 
-        // TODO: send email here
+            // TODO: send email here
 
-        $inquiry = new Inquiry();
-        $inquiry->first_name = $validatedData['first_name'];
-        $inquiry->last_name = $validatedData['last_name'];
-        $inquiry->email = $validatedData['email'];
-        $inquiry->subject = $validatedData['subject'];
-        $inquiry->message = $validatedData['message'];
-        $inquiry->save();
+            $inquiry = new Inquiry();
+            $inquiry->first_name = $validatedData['first_name'];
+            $inquiry->last_name = $validatedData['last_name'];
+            $inquiry->email = $validatedData['email'];
+            $inquiry->subject = $validatedData['subject'];
+            $inquiry->message = $validatedData['message'];
+            $inquiry->save();
 
-        // Mail::to($inquiry->email)->send(new InquirySubmitted($inquiry));
+            // Mail::to($inquiry->email)->send(new InquirySubmitted($inquiry));
 
 
-        return redirect()->back()->with('success', 'Your message has been sent successfully!');
+            return redirect()->back()->with('success', 'Your message has been sent successfully!');
+
+        } catch (\Throwable $th) {
+            return redirect()->back()->with('error', 'There was a problem with your request. Please check the details and try again');
+        }
     }
 
     /**
